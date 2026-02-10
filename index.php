ef7c9d38b3810cfa67b94c4bba690979aefd96c5
mdraushan <mdraushanji22@gmail.com>
2026-02-10 21:30:08 +0530

    php-chatApp

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <?php
-include('header.php');
-?>
+session_start();
+if(isset($_SESSION["userName"]) && isset($_SESSION["phone"])){
 
-<h1>Pusher Test</h1>
-<p>
-    Try publishing an event to channel <code>my-channel</code>
-    with event name <code>my-event</code>.
-</p>
-<?php
-include('footer.php');
 ?>
-<script>
-    var channel = pusher.subscribe('my-channel');
-    channel.bind('my-event', function(data) {
-        alert(JSON.stringify(data));
-    });
-</script>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ChatRoom</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <h1>ChatRoom</h1>
+  <div class="chat">
+    <h2>Welcome to <span><?= $_SESSION["userName"]?></span></h2>
+    <div class="msg">
+      
+
+
+    </div>
+    <div class="input_msg">
+      <input type="text" placeholder="Write msg Here" id="input_msg">
+      <button onclick="update()">Send</button>
+    </div>
+  </div>
+</body>
+<script src="js/script.js"></script>
+
+</html>
+
+<?php
+}else{
+
+  header("location: login.php");
+
+
+}
+?>
